0a0474e0885f03141972afb1ffde04e8b5e076cf
Nicky Ngailo <nickyrabit@gmail.com>
2019-11-29 18:43:52 +0300

    adding more commment in livedata

--- app/src/main/java/com/nickyrabit/roomwithlivedata/room_db/dao_interfaces/PetrolPriceDao.java
+++ app/src/main/java/com/nickyrabit/roomwithlivedata/room_db/dao_interfaces/PetrolPriceDao.java
@@ -32,7 +32,14 @@ public interface PetrolPriceDao {
     * I have set a LIVEDATA on
     *  PetrolPriceList which has the task of alerting all Observer object
     * when there is a change in a particular data in our case,
-    * PetrolPrice.*/
+    * PetrolPrice.
+    *
+    *
+    * A quick explanation on code below,
+    *  @Query() is an annotation that will hold all SQLite queries
+    *  because Room can perform only some few basic queries
+    * without typing them.
+    * */
         @Query("SELECT * FROM 'PetrolPrice'")
     LiveData<List<PetrolPrice>> getAllPrices();
 
